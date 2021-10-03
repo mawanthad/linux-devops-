5921952780fc5c9386541b5ee857341d1b434933
mawanthad <87687389+mawanthad@users.noreply.github.com>
2021-10-03 21:10:16 +0530

    Update task02.sh

--- Ansible/task02.sh
+++ Ansible/task02.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-sudo ansible webserver1 -m hostname -a name='LT-2021-007-webserver1'
-sudo ansible webserver2 -m hostname -a name='LT-2021-007-webserver2'
+sudo ansible webserver1 -m hostname -a name='LT-2021-011-webserver1' 
+sudo ansible webserver2 -m hostname -a name='LT-2021-011-webserver2'
+
